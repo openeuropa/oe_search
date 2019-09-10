cf0ddc351cb39dd1d8f09860dbf7082b3549ca58
Imanol <ier@opendeusto.es>
2019-09-10 12:54:41 +0100

    OPENEUROPA-2286: Add scenario tags.

--- tests/Behat/SearchContext.php
+++ tests/Behat/SearchContext.php
@@ -4,6 +4,8 @@ declare(strict_types = 1);
 
 namespace Drupal\Tests\oe_search\Behat;
 
+use Behat\Behat\Hook\Scope\AfterScenarioScope;
+use Behat\Behat\Hook\Scope\BeforeScenarioScope;
 use Drupal\DrupalExtension\Context\RawDrupalContext;
 use PHPUnit\Framework\Assert;
 
@@ -13,12 +15,45 @@ use PHPUnit\Framework\Assert;
 class SearchContext extends RawDrupalContext {
 
   /**
+   * Prevent redirections for the whole scenario.
+   *
+   * @param \Behat\Behat\Hook\Scope\BeforeScenarioScope $scope
+   *   The Hook scope.
+   *
+   * @BeforeScenario @no-redirects
+   */
+  public function preventRedirects(BeforeScenarioScope $scope): void {
+    $this->iDoNotFollowRedirects();
+  }
+
+  /**
+   * Allow redirections after th escenario.
+   *
+   * @param \Behat\Behat\Hook\Scope\AfterScenarioScope $scope
+   *   The Hook scope.
+   *
+   * @AfterScenario @no-redirects
+   */
+  public function allowRedirects(AfterScenarioScope $scope): void {
+    $this->iFollowRedirects();
+  }
+
+  /**
    * Prevent redirects so we can check their target before they happen.
    *
    * @When /^I do not follow redirects$/
    */
   public function iDoNotFollowRedirects() {
     $this->getSession()->getDriver()->getClient()->followRedirects(FALSE);
+  }
+
+  /**
+   * Allow redirects during tests (usually after we have prevented them first).
+   *
+   * @When /^I follow redirects$/
+   */
+  public function iFollowRedirects() {
+    $this->getSession()->getDriver()->getClient()->followRedirects(TRUE);
   }
 
   /**
--- tests/features/search-block.feature
+++ tests/features/search-block.feature
@@ -4,9 +4,9 @@ Feature: Search block
   As an anonymous user
   I want to search on ec.europa.eu by using the search block
 
+  @no-redirects
   Scenario: I am redirected to the ec.europa.eu search results page when I use the search block
-    Given I do not follow redirects
-    And the following languages are available:
+    Given the following languages are available:
       | languages |
       | en        |
       | fr        |
