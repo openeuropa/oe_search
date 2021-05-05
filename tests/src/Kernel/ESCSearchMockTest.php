27b8c98ffec708ebf2efef05391f265a821f90dc
Bachir Benbahloul <bachirbenbahloul@gmail.com>
2021-05-05 16:34:10 +0200

    OEL-102: Mock Search Api service call in tests

--- tests/src/Kernel/ESCSearchMockTest.php
+++ tests/src/Kernel/ESCSearchMockTest.php
@@ -4,6 +4,8 @@ namespace Drupal\Tests\oe_search\Kernel;
 
 use Drupal\KernelTests\KernelTestBase;
 use Drupal\oe_search\EuropaSearchService;
+use OpenEuropa\EuropaSearchClient\Model\Document;
+use OpenEuropa\EuropaSearchClient\Model\Search;
 
 /**
  * Tests the HTTP layer mocking.
@@ -21,11 +23,41 @@ class ESCSearchMockTest extends KernelTestBase {
    * Tests Europa Search Client Search.
    */
   public function testSearchApi(): void {
-    $controller = $this->getMockBuilder(EuropaSearchService::class)
+    $document = new Document();
+    $document->setAccessRestriction('true');
+    $document->setApiVersion('string');
+    $document->setContent('string');
+    $document->setContent('string');
+    $document->setDatabase('string');
+    $document->setDatabaseLabel('string');
+    $document->setGroupById('string');
+    $document->setLanguage('string');
+    $document->setMetadata([
+      'additionalProp1' => ["string"],
+      'additionalProp2' => ["string"]
+    ]);
+    $document->setPages(0);
+    $document->setReference('string');
+    $document->setSummary('string');
+    $document->setTitle('string');
+    $document->setUrl('string');
+    $document->setWeight(0);
+
+    $documents = [$document];
+
+    // Mock EuropaSearchService service method searchApi
+    $serviceMock = $this->getMockBuilder(EuropaSearchService::class)
       ->disableOriginalConstructor()
       ->getMock();
 
-    $this->assertNull($this->container->get('plugin.manager.service_mock')->getMatchingPlugin($controller->searchApi(), []));
+    $serviceMock->expects($this->any())
+      ->method('searchApi')
+      ->will($this->returnValue($documents));
+
+    $expected = new Search();
+    $expected->setResults($documents);
+
+    $this->assertEquals($expected->getResults(), $serviceMock->searchApi());
   }
 
   /**
