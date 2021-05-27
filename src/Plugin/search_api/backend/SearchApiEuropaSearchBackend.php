c7478302935641e630530789ac84b767dbb6187c
Claudiu Cristea <clau.cristea@gmail.com>
2021-05-27 15:19:57 +0300

    OEL-149: Allow multi-site & multi-index search/ingestion.

--- src/Plugin/search_api/backend/SearchApiEuropaSearchBackend.php
+++ src/Plugin/search_api/backend/SearchApiEuropaSearchBackend.php
@@ -279,4 +279,44 @@ class SearchApiEuropaSearchBackend extends BackendPluginBase implements PluginFo
     $this->messenger()->addError($this->renderer->render($error));
   }
 
+  /**
+   * Creates an ID used as the unique identifier at the Europa Search server.
+   *
+   * This method should be used everywhere we need to get the Europa Search
+   * reference for a given Search API item ID. The way it's constructed
+   * guarantees that we can ingest content from different sites and indexes in
+   * the same Europa Search database.
+   *
+   * @param string $index_id
+   *   The index ID.
+   * @param string $item_id
+   *   The item ID.
+   *
+   * @return string
+   *   A unique Europa Search reference for the given item.
+   */
+  protected function createReference(string $index_id, string $item_id): string {
+    return "{$this->getSiteHash()}-{$index_id}-{$item_id}";
+  }
+
+  /**
+   * Returns a unique hash for the current site.
+   *
+   * This is used to identify the Europa Search documents from different sites
+   * within a single Europa Search database.
+   *
+   * @return string
+   *   A unique site hash, containing only alphanumeric characters.
+   */
+  protected function getSiteHash(): string {
+    // Copied from \Drupal\search_api_solr\Utility\Utility::getSiteHash().
+    $state = \Drupal::state();
+    if (!$hash = $state->get('oe_search.site_hash')) {
+      global $base_url;
+      $hash = substr(base_convert(hash('sha256', uniqid($base_url, TRUE)), 16, 36), 0, 6);
+      $state->set('oe_search.site_hash', $hash);
+    }
+    return $hash;
+  }
+
 }
